3cefb4dcb241aabfc39eb03170034e5c4e9c249f
zwq-qianyu <1347638091@qq.com>
2018-10-02 15:36:00 +0800

    将学生端和企业端融入到2.0版本中

--- csu_jobsky_collect/csu_jobsky_collect/urls.py
+++ csu_jobsky_collect/csu_jobsky_collect/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^', include('myapp.urls')),
     url(r'^myadmin/', include('myadmin.urls')),
+    url(r'^', include('web.urls')),
 ]
