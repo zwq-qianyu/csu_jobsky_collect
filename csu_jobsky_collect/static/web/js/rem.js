942122669ce27efab4b2666f200a46280f2e32a6
zwq-qianyu <1347638091@qq.com>
2018-10-31 12:35:00 +0800

    已经完成所有需要修改的地方

--- csu_jobsky_collect/static/web/js/rem.js
+++ csu_jobsky_collect/static/web/js/rem.js
@@ -5,8 +5,7 @@
 		// 判断 当前文档宽度 大于 750 那么变量就是 750,否则就是当前文档实际宽度
 		var clientWidthValue = html.clientWidth > 750 ? 750 : html.clientWidth;
 		//设置当前文档的字体大小
-		html.style.fontSize = 20*(clientWidthValue/375)+'px';
-
+		html.style.fontSize = 25*(clientWidthValue/375)+'px';
 	}
 	calc();
 	//绑定事件,当浏览器的大小发生改变时
